58a2355fb41cccfa9d04f9db8d4559aec4fae99e
Grossmann <stefan.grossmann@agfa.com>
2018-01-12 14:56:58 +0100

    Use javax.cache.annotation.CacheResult annotation

--- src/main/java/stefan/demo/jcache/control/DemoCachedService.java
+++ src/main/java/stefan/demo/jcache/control/DemoCachedService.java
@@ -1,22 +1,17 @@
 package stefan.demo.jcache.control;
 
-import org.infinispan.Cache;
 import stefan.demo.jcache.entity.Message;
 
-import javax.inject.Inject;
+import javax.cache.annotation.CacheResult;
+import javax.enterprise.context.Dependent;
 import java.util.UUID;
 
+@Dependent
 public class DemoCachedService {
-    @Inject
-    private Cache<String, Message> cache;
 
+    @CacheResult
     public Message getMessage(String to) {
-        Message message = cache.get(to);
-        if (message == null) {
-            final String text = String.format("Hello %s! [%s]", to, UUID.randomUUID().toString());
-            message = new Message(text);
-            cache.putForExternalRead(to, message);
-        }
-        return message;
+        final String text = String.format("Hello %s! [%s]", to, UUID.randomUUID().toString());
+        return new Message(text);
     }
 }
